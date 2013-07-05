mitsukarenai/Projet-Autoblog

commit 8e8797457d9a426329c8a63ec7248ade37663617

Fixes #5

index.php
@@ -861,7 +861,7 @@ if( !empty($_POST['opml_file']) && ALLOW_NEW_AUTOBLOGS && ALLOW_NEW_AUTOBLOGS_BY
             foreach($subdirs as $unit)
             {
                 if(!is_dir($unit) || file_exists( $unit . '/index.html' ) || file_exists( $unit . '/index.htm' ) || file_exists( $unit . '/index.php' ) ) {
-                    $docs[] = '<a href="'. $unit . '">'. substr($unit, (strrpos($unit, '/')) + 1 ) .'</a>';
+                    $docs[] = '<a href="'. urlencode($unit) . '">'. substr($unit, (strrpos($unit, '/')) + 1 ) .'</a>';
                }
             }
         }

.gitignore
@@ -7,3 +7,5 @@ resources/rss.json
 resources/user.css
 autoblogs/*
 !autoblogs/autoblog.php
+docs/*
+!docs/docs.txt

commit 9a125ef44844bde0c19ff94c0529f80b785a218a

Fixes regression introduced by 8e8797457d9a426329c8a63ec7248ade37663617

index.php
@@ -914,7 +914,7 @@ if( !empty($_POST['opml_file']) && ALLOW_NEW_AUTOBLOGS && ALLOW_NEW_AUTOBLOGS_BY
             foreach($subdirs as $unit)
             {
                 if(!is_dir($unit) || file_exists( $unit . '/index.html' ) || file_exists( $unit . '/index.htm' ) || file_exists( $unit . '/index.php' ) ) {
-                    $docs[] = '<a href="'. urlencode($unit) . '">'. substr($unit, (strrpos($unit, '/')) + 1 ) .'</a>';
+                    $docs[] = '<a href="'. preg_replace('~ ~', '%20', $unit) . '">'. substr($unit, (strrpos($unit, '/')) + 1 ) .'</a>';
                }
             }
         }